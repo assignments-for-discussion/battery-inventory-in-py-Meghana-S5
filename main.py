7f7da6853c92d9f2c98b7952e1ad7ff2da083518
Meghana-S5 <149789789+Meghana-S5@users.noreply.github.com>
2023-11-03 17:43:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-
 def count_batteries_by_health(present_capacities):
+  SOH=100*present_capacity//120
+  for i in present_capacities:
+    if SOH>=80 and SOH<=100:
+      health_count+=1
+    elif SOH>=62 and SOH<=80:
+      exchange_count+=1
+    else:
+      fail_count+=1
+
   return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
+    "healthy": health_count,
+    "exchange": exchange_count,
+    "failed": fail_count
   }
 
 
